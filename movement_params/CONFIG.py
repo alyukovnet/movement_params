e005f3b4db8dc354f07f384b97c18d84db2cf1da
maaxxaam <maaxxaam@protonmail.com>
2022-05-12 16:29:39 +0300

    Change CONFIG.py to use defaults

--- movement_params/CONFIG.py
+++ movement_params/CONFIG.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
-from numpy import ndarray
+
+from numpy import ndarray, array
 
 from movement_params.io import Input, Output, StreamInput, WindowOutput, VideoFileInput
 from movement_params.frame_processors import FrameProcessor, ObjectsDetector, ObjectsTracker, ObjectsIdentifier, \
@@ -11,10 +12,11 @@ from movement_params.frame_processors import FrameProcessor, ObjectsDetector, Ob
 @dataclass
 class Config:
     input_type: Input
-    output_type: Optional[Output]
-    processors: list[FrameProcessor]
-    camera_matrix: Optional[ndarray]
-    world_matrix: Optional[ndarray]
+    output_type: Optional[Output] = field(default_factory=lambda: WindowOutput())
+    world_matrix: Optional[ndarray] = field(default_factory=lambda: array([[0, 0], [1, 0], [1, 1], [0, 1]]))
+    camera_matrix: Optional[ndarray] = field(default_factory=lambda: array([[0, 0], [1, 0], [1, 1], [0, 1]]))
+    aruco_ids: Optional[ndarray] = field(default_factory=lambda: array([128, 129, 130, 131]))
+    processors: list[FrameProcessor] = field(default_factory=lambda: processors)
 
 
 processors = [
@@ -22,29 +24,25 @@ processors = [
     MovementDetector(),
     ObjectsTracker(),
     # ObjectsIdentifier(),
-    # PositionCalculator(),
+    PositionCalculator(),
     # ParamsCalculator(),
 ]
 
 
 def DefaultConfig() -> Config:
     """
-    Default config
+    Default config. Uses camera to apply algorithms in real-time.
     """
     return Config(
         input_type=StreamInput(),  # Camera 0
-        output_type=WindowOutput(),
-        processors=processors
     )
 
 
 def DebugConfig() -> Config:
     """
-    Debug config
+    Debug config. Uses a predetermined video file.
     """
     return Config(
         input_type=StreamInput('./cars1.mp4'),  # Video file with frames skipping
         # input_type=VideoFileInput('./cars1.mp4'),  # Frame-by-frame video input
-        output_type=WindowOutput(),
-        processors=processors
     )
